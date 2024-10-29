7eaa6fe0c9d71b5a1c2d2c9a28811295c9c48698
Omar Alvarez <omar.alvarez@udc.es>
2024-10-29 22:44:03 +0100

    Housing visualization generation.

---
+++ test_housing_baseline_correlation.py
@@ -0,0 +1,110 @@
+from gentab.generators import (
+    SMOTE,
+    ADASYN,
+    TVAE,
+    CTGAN,
+    GaussianCopula,
+    CopulaGAN,
+    CTABGAN,
+    CTABGANPlus,
+    AutoDiffusion,
+    ForestDiffusion,
+    Tabula,
+    GReaT,
+)
+from gentab.data import Config, Dataset
+from gentab.utils import console
+
+config = Config("configs/california_housing_cr.json")
+
+dataset = Dataset(config)
+
+labels = ["lowest", "lower", "low", "medium", "high", "higher", "highest"]
+bins = [float("-inf"), 0.7, 1.4, 2.1, 2.8, 3.5, 4.2, float("inf")]
+
+dataset.create_bins(bins, labels)
+
+n_samples = dataset.class_counts().to_dict()
+
+console.print(dataset.class_counts(), dataset.row_count())
+generator = TVAE(dataset)
+generator.generate(n_samples=n_samples, append=False)
+dataset.save_to_disk(generator)
+console.print(dataset.generated_class_counts(), dataset.generated_row_count())
+
+console.print(dataset.class_counts(), dataset.row_count())
+generator = CTGAN(dataset)
+generator.generate(n_samples=n_samples, append=False)
+dataset.save_to_disk(generator)
+console.print(dataset.generated_class_counts(), dataset.generated_row_count())
+
+console.print(dataset.class_counts(), dataset.row_count())
+generator = GaussianCopula(dataset)
+generator.generate(n_samples=n_samples, append=False)
+dataset.save_to_disk(generator)
+console.print(dataset.generated_class_counts(), dataset.generated_row_count())
+
+console.print(dataset.class_counts(), dataset.row_count())
+generator = CopulaGAN(dataset)
+generator.generate(n_samples=n_samples, append=False)
+dataset.save_to_disk(generator)
+console.print(dataset.generated_class_counts(), dataset.generated_row_count())
+
+console.print(dataset.class_counts(), dataset.row_count())
+generator = CTABGAN(
+    dataset,
+    test_ratio=0.10,
+)
+generator.generate(n_samples=n_samples, append=False)
+console.print(dataset.generated_class_counts(), dataset.generated_row_count())
+dataset.save_to_disk(generator)
+
+console.print(dataset.class_counts(), dataset.row_count())
+generator = CTABGANPlus(
+    dataset,
+    test_ratio=0.10,
+)
+generator.generate(n_samples=n_samples, append=False)
+console.print(dataset.generated_class_counts(), dataset.generated_row_count())
+dataset.save_to_disk(generator)
+
+console.print(dataset.class_counts(), dataset.row_count())
+generator = AutoDiffusion(dataset)
+generator.generate(n_samples=n_samples, append=False)
+console.print(dataset.generated_class_counts(), dataset.generated_row_count())
+dataset.save_to_disk(generator)
+
+console.print(dataset.class_counts(), dataset.row_count())
+generator = ForestDiffusion(dataset, n_jobs=1, duplicate_K=4, n_estimators=100)
+generator.generate(n_samples=n_samples, append=False)
+console.print(dataset.generated_class_counts(), dataset.generated_row_count())
+dataset.save_to_disk(generator)
+
+console.print(dataset.class_counts(), dataset.row_count())
+generator = GReaT(
+    dataset,
+    epochs=15,
+    max_length=2000,
+    temperature=0.6,
+    batch_size=32,
+    max_tries_per_batch=4096,
+    n_samples=8192,
+)
+generator.generate(n_samples=n_samples, append=False)
+console.print(dataset.generated_class_counts(), dataset.generated_row_count())
+dataset.save_to_disk(generator)
+
+console.print(dataset.class_counts(), dataset.row_count())
+generator = Tabula(
+    dataset,
+    # categorical_columns=[dataset.config["y_label"]],
+    epochs=15,
+    max_length=1024,
+    temperature=0.6,
+    batch_size=32,
+    max_tries_per_batch=4096,
+    n_samples=8192,
+)
+generator.generate(n_samples=n_samples, append=False)
+console.print(dataset.generated_class_counts(), dataset.generated_row_count())
+dataset.save_to_disk(generator)
